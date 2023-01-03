9fcc4961cc0082458c774f5579c844865056b1ad
Younghyun <yhbae97@gmail.com>
2023-01-03 19:18:42 +0100

    add checkbox and delete btn logic

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import AddTodo from '../AddTodo/AddTodo';
+import Todo from '../Todo/Todo';
 
 const TodoList = () => {
   const [todos, setTodos] = useState([
@@ -7,13 +8,23 @@ const TodoList = () => {
     {id: '02', text: 'studying', status: 'active'},
   ]);
   const handleAdd = (todo) => setTodos([...todos, todo]);
+  const handleUpdate = (updated) => 
+    setTodos(todos.map((t) => (t.id === updated.id ? updated : t)));
+  const handleDelete = (deleted) => 
+    setTodos(todos.filter((t) => (t.id !== deleted.id)));
+  
 
   return (
     <section>
       <ul>
         {
           todos.map((item) => (
-            <li key={item.id}>{item.text}</li>
+            <Todo
+              key={item.id} 
+              todo={item} 
+              onUpdate={handleUpdate} 
+              onDelete={handleDelete} 
+            />
           ))
         }
       </ul>
